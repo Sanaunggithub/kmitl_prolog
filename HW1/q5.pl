66420225b524cc1f3650c899b447ad15cfdf8b75
Sanaunggithub <usasanaung2000@gmail.com>
2026-08-12 14:45:12 +0700

    add q5.pl

--- HW1/q5.pl
+++ HW1/q5.pl
@@ -1,3 +1,17 @@
-find_max([],[], M).
+max_of_list([X],X).
 
-find_max([H|T], L2,)
+max_of_list([H|T], H):-
+    max_of_list(T, MaxT),
+    H > MaxT.
+
+max_of_list([H|T], MaxT):-
+    max_of_list(T, MaxT),
+    H =< MaxT.
+
+find_max([],[], Max).
+
+
+find_max(L1, L2, Max):-
+    max_of_list(L1, Max1),
+    max_of_list(L2, Max2),
+    Max is max(Max1, Max2).
