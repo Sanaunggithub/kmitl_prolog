89e98ac408fe512f59093ba4f49a1e49f9ee8685
Sanaunggithub <usasanaung2000@gmail.com>
2026-08-17 23:04:47 +0700

    add fix Lab2/q2

--- Lab 2/q2.pl
+++ Lab 2/q2.pl
@@ -4,4 +4,4 @@ point(p1, 4, 6).
 final_distance(PointA, PointB, Dist) :-
     point(PointA, X1, Y1),
     point(PointB, X2, Y2),
-    Dist is sqrt((X2-X1)**2 + (Y2-Y1)**2).s
+    Dist is sqrt((X2-X1)**2 + (Y2-Y1)**2).
